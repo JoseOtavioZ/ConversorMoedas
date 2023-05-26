3e5380ef1a2e4a85ce4281c62a82f57ab33d0483
JoseOtavioZ <joseotz97@gmail.com>
2023-05-26 19:36:32 -0300

    Update remover main redundante

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Valor form = new Valor();
+        form.setContentPane(form.formValor);
+        form.setVisible(true);
+        form.setSize(500, 500);
+        form.setTitle("Conversor de moeda");
     }
 }
